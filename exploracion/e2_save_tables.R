9efb6d3136fb6f6732a53e5044578b4180b4933c
PJaramilloV <pablojaramillovesperinas@gmail.com>
2022-05-19 16:24:31 -0400

    cambio de ruta de guardado de tablas originales

--- exploracion/e2_save_tables.R
+++ exploracion/e2_save_tables.R
@@ -1,10 +1,10 @@
 
 
-write.csv(casearrest,     paste0(getwd(),'/data/casearrest'   , '.csv'))
-write.csv(charge,         paste0(getwd(),'/data/charge'       , '.csv'))
-write.csv(compas,         paste0(getwd(),'/data/compas'       , '.csv'))
-write.csv(jailhistory,    paste0(getwd(),'/data/jailhistory'  , '.csv'))
-write.csv(people,         paste0(getwd(),'/data/people'       , '.csv'))
-write.csv(people_charge,  paste0(getwd(),'/data/people_charge', '.csv'))
-write.csv(prisonhistory,  paste0(getwd(),'/data/prisonhistory', '.csv'))
-write.csv(summary,        paste0(getwd(),'/data/summary'      , '.csv'))
+write.csv(casearrest,     paste0(getwd(),'/data/original/casearrest'   , '.csv'))
+write.csv(charge,         paste0(getwd(),'/data/original/charge'       , '.csv'))
+write.csv(compas,         paste0(getwd(),'/data/original/compas'       , '.csv'))
+write.csv(jailhistory,    paste0(getwd(),'/data/original/jailhistory'  , '.csv'))
+write.csv(people,         paste0(getwd(),'/data/original/people'       , '.csv'))
+write.csv(people_charge,  paste0(getwd(),'/data/original/people_charge', '.csv'))
+write.csv(prisonhistory,  paste0(getwd(),'/data/original/prisonhistory', '.csv'))
+write.csv(summary,        paste0(getwd(),'/data/original/summary'      , '.csv'))
